04c3c91873ce07f98ab5f5b0b30a52bb08e46933
asb95505009 <55088875+asb95505009@users.noreply.github.com>
2019-11-07 11:15:08 +0800

    Update main.c
    code reading.

--- examples/k4arecord_custom_track/main.c
+++ examples/k4arecord_custom_track/main.c
@@ -49,29 +49,29 @@ void fill_bitmap_header(uint32_t width, uint32_t height, BITMAPINFOHEADER *out)
     out->biClrUsed = 0;
     out->biClrImportant = 0;
 }
-
-int main(int argc, char **argv)
+//------------------------------------------------------------------------------------codec tuned?
+int main(int argc, char **argv)                
 {
-    if (argc < 2)
+    if (argc < 2)                                               //it seems not necessary
     {
         printf("Usage: k4arecord_custom_track output.mkv\n");
         exit(0);
     }
 
-    char *recording_filename = argv[1];
+    char *recording_filename = argv[1];                         //argv stores recording filename
 
-    k4a_device_configuration_t device_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
+    k4a_device_configuration_t device_config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;      //setup config
     device_config.depth_mode = K4A_DEPTH_MODE_NFOV_UNBINNED;
     device_config.camera_fps = K4A_FRAMES_PER_SECOND_30;
 
     k4a_device_t device;
-    VERIFY(k4a_device_open(0, &device));
-    VERIFY(k4a_device_start_cameras(device, &device_config));
+    VERIFY(k4a_device_open(0, &device));                        //open device
+    VERIFY(k4a_device_start_cameras(device, &device_config));   //start camera
 
     printf("Device started\n");
 
-    k4a_record_t recording;
-    if (K4A_FAILED(k4a_record_create(recording_filename, device, device_config, &recording)))
+    k4a_record_t recording;                                                                         //1.initialize recording sample
+    if (K4A_FAILED(k4a_record_create(recording_filename, device, device_config, &recording)))       //2.create recording sample
     {
         printf("Unable to create recording file: %s\n", recording_filename);
         return 1;
@@ -89,54 +89,60 @@ int main(int argc, char **argv)
 
     // Add a custom video track to store processed depth images.
     // Read the depth resolution from the camera configuration so we can create our custom track with the same size.
-    k4a_calibration_t sensor_calibration;
-    VERIFY(k4a_device_get_calibration(device, device_config.depth_mode, K4A_COLOR_RESOLUTION_OFF, &sensor_calibration));
-    uint32_t depth_width = (uint32_t)sensor_calibration.depth_camera_calibration.resolution_width;
+    k4a_calibration_t sensor_calibration;                                                           //3.initialize custom video track
+    VERIFY(k4a_device_get_calibration(device, device_config.depth_mode, K4A_COLOR_RESOLUTION_OFF, &sensor_calibration));//4.create custom video track
+    uint32_t depth_width = (uint32_t)sensor_calibration.depth_camera_calibration.resolution_width;    //5.setting custom video track
     uint32_t depth_height = (uint32_t)sensor_calibration.depth_camera_calibration.resolution_height;
 
-    BITMAPINFOHEADER codec_header;
-    fill_bitmap_header(depth_width, depth_height, &codec_header);
+    BITMAPINFOHEADER codec_header;                                      //6. initialize codec_header  
+    fill_bitmap_header(depth_width, depth_height, &codec_header);       //7. create header
 
-    k4a_record_video_settings_t video_settings;
-    video_settings.width = depth_width;
+    k4a_record_video_settings_t video_settings;                         //8. initialize record format setting
+    video_settings.width = depth_width;                                 //9. setting record format setting
     video_settings.height = depth_height;
     video_settings.frame_rate = 30; // Should be the same rate as device_config.camera_fps
 
-    // Add the video track to the recording.
-    VERIFY(k4a_record_add_custom_video_track(recording,
-                                             "PROCESSED_DEPTH",
+    // Add the video track to the recording.                            //think recording as one carrier = video + tag +...
+    VERIFY(k4a_record_add_custom_video_track(recording,                 //10. add video track to the recording
+                                             "PROCESSED_DEPTH",         
                                              "V_MS/VFW/FOURCC",
                                              (uint8_t *)(&codec_header),
                                              sizeof(codec_header),
                                              &video_settings));
 
-    // Write the recording header now that all the track metadata is set up.
-    VERIFY(k4a_record_write_header(recording));
+    // Write the recording header now that all the track metadata is set up.   //11.write header 
+    VERIFY(k4a_record_write_header(recording));                                //setup completed
 
-    // Start reading 100 depth frames (~3 seconds at 30 fps) from the camera.
+    
+    
+    
+    
+    
+    
+    // Start reading 100 depth frames (~3 seconds at 30 fps) from the camera.   //reading frames from camera to recording
     for (int frame = 0; frame < 100; frame++)
     {
-        k4a_capture_t capture;
-        k4a_wait_result_t get_capture_result = k4a_device_get_capture(device, &capture, K4A_WAIT_INFINITE);
+        k4a_capture_t capture;                                                  //1. initialize capture
+        k4a_wait_result_t get_capture_result = k4a_device_get_capture(device, &capture, K4A_WAIT_INFINITE);//2. create capture
         if (get_capture_result == K4A_WAIT_RESULT_SUCCEEDED)
         {
             // Write the capture to the built-in tracks
-            VERIFY(k4a_record_write_capture(recording, capture));
+            VERIFY(k4a_record_write_capture(recording, capture));               //3. write capture to recording
 
             // Get the depth image from the capture so we can write a processed copy to our custom track.
-            k4a_image_t depth_image = k4a_capture_get_depth_image(capture);
-            if (depth_image)
+            k4a_image_t depth_image = k4a_capture_get_depth_image(capture);     //4. get depth image from capture --> put into recording
+            if (depth_image)                                                    
             {
                 // The YUY2 image format is the same stride as the 16-bit depth image, so we can modify it in-place.
-                uint8_t *depth_buffer = k4a_image_get_buffer(depth_image);
-                size_t depth_buffer_size = k4a_image_get_size(depth_image);
-                for (size_t i = 0; i < depth_buffer_size; i += 2)
+                uint8_t *depth_buffer = k4a_image_get_buffer(depth_image);      //5.change depth image to buffer
+                size_t depth_buffer_size = k4a_image_get_size(depth_image);     //6.get size of depth image
+                for (size_t i = 0; i < depth_buffer_size; i += 2)               //7. "for-loop" turn depth value into color format
                 {
                     // Convert the depth value (16-bit, in millimeters) to the YUY2 color format.
                     // The YUY2 format should be playable in video players such as VLC.
-                    uint16_t depth = (uint16_t)(depth_buffer[i + 1] << 8 | depth_buffer[i]);
+                    uint16_t depth = (uint16_t)(depth_buffer[i + 1] << 8 | depth_buffer[i]);    //depth to color format
                     // Clamp the depth range to ~1 meter and scale it to fit in the Y channel of the image (8-bits).
-                    if (depth > 0x3FF)
+                    if (depth > 0x3FF)                                                          
                     {
                         depth_buffer[i] = 0xFF;
                     }
@@ -148,16 +154,16 @@ int main(int argc, char **argv)
                     depth_buffer[i + 1] = 128;
                 }
 
-                VERIFY(k4a_record_write_custom_track_data(recording,
+                VERIFY(k4a_record_write_custom_track_data(recording,            //8.write YUY2 color video format into recording
                                                           "PROCESSED_DEPTH",
                                                           k4a_image_get_device_timestamp_usec(depth_image),
                                                           depth_buffer,
                                                           (uint32_t)depth_buffer_size));
 
-                k4a_image_release(depth_image);
+                k4a_image_release(depth_image);                 //9.release depth image at the end of depth image handling
             }
 
-            k4a_capture_release(capture);
+            k4a_capture_release(capture);                       //10.release capture
         }
         else if (get_capture_result == K4A_WAIT_RESULT_TIMEOUT)
         {
@@ -172,14 +178,14 @@ int main(int argc, char **argv)
         }
     }
 
-    k4a_device_stop_cameras(device);
+    k4a_device_stop_cameras(device);                            //11.stop camera
 
     printf("Saving recording...\n");
-    VERIFY(k4a_record_flush(recording));
-    k4a_record_close(recording);
+    VERIFY(k4a_record_flush(recording));                        //12.flush data into recording
+    k4a_record_close(recording);                                //13.release recording
 
     printf("Done\n");
-    k4a_device_close(device);
+    k4a_device_close(device);                                   //14.release device
 
     return 0;
 }
